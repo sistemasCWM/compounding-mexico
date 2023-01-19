be83628d1c1257da9fa0ba23917cf0ba181ab6d8
lars-odoo <lars@odoo.com>
2023-01-19 09:26:18 -0800

    [MOD] General modifications done.

--- compounding_mexico_digest_kpis/models/digest.py
+++ compounding_mexico_digest_kpis/models/digest.py
@@ -28,59 +28,12 @@ class Digest(models.Model):
     KPI no. 1
         Confirmed Sales by Salesperson
             Showing a list with two columns. The column on the left contains the name of the salesperson and the column on the right total amount of his/her confirmed sales.
-    """
-    # kpi_quote = fields.Boolean('Confirmed Sales by Salesperson')
-    kpi_quote = fields.Boolean('Confirmed sales')
-    kpi_quote_value = fields.Integer(compute="_compute_kpi_quote_value")
 
-    def _compute_kpi_quote_value(self):
-        # self.kpi_quote_value = 123
-        for record in self:
-            start, end, company = record._get_kpi_compute_parameters()
-            quote = self.env['sale.order'].search_count([('state', '=', 'sent'), ('date_order', '>=', start), ('date_order', '<', end)])
-            record.kpi_quote_value = quote
-
-    """
     2) Feature 2
     KPI no. 2
         Confirmed sales of new clients
             Show an aggregate of all the confirmed sales from new clients
-    """
-    # kpi_newclient = fields.Boolean(compute="_compute_kpi_newclient_value"),def _compute_kpi_newclient_value()(self):,print('456')
-
-    # kpi_new_client = fields.Boolean('Confirmed sales of new clients')
-    # kpi_new_client = fields.Boolean('Confirmed New Sales')
-    kpi_new_client = fields.Boolean('Confirmed sales to new clients')
-    kpi_new_client_value = fields.Integer(compute="_compute_kpi_new_client_value")
-
-    def _compute_kpi_new_client_value(self):
-        
-        # self.kpi_new_client_value = 456
-
-        # start_datetime = datetime.utcnow() + relativedelta(days=-1)
-        # end_datetime = datetime.utcnow() + relativedelta(days=+1)
-
-        start_datetime = fields.Datetime.to_string(self._context.get('start_datetime') + relativedelta(days=-1))
-        end_datetime = fields.Datetime.to_string(self._context.get('end_datetime') + relativedelta(days=+1))
-
-        client_ids = self.env['res.partner'].search_count([('create_date', '>=', start_datetime), ('create_date', '<=', end_datetime)])
-
-        # print(type(client_ids))
-
-        # if type(client_ids) == "<class 'int'>":
-        if type(client_ids) == int:
-            client_ids = [client_ids]
-
-        # print(client_ids)
-
-        for record in self:
-            start, end, company = record._get_kpi_compute_parameters()
-            new_client = self.env['sale.order'].search_count([('state', '=', 'sent'), ('date_order', '>=', start), ('date_order', '<', end), ('partner_id', 'in', client_ids)])
-            # print(new_client)
-            # print(new_client)
-            record.kpi_new_client_value = new_client
     
-    """
     3) Feature 3
     KPI no.3
     Bank account balance in General Ledger
@@ -89,82 +42,54 @@ class Digest(models.Model):
         Be ready to not show previous accounts if they get deleted.
     """
 
-    # kpi_bank_account = fields.Boolean('Bank account balance in General Ledger')
-    # kpi_bank_account_value = fields.Integer(compute="_compute_kpi_bank_account_value")
+    # KPI
+    kpi_quote = fields.Boolean('Confirmed sales')
+    kpi_quote_value = fields.Integer(compute="_compute_kpi_quote_value")
 
-    def _compute_kpi_bank_account_value(self):
-        
-        """
-        # self.kpi_bank_account_value = 789
+    def _compute_kpi_quote_value(self):
+        for record in self:
+            start, end, company = record._get_kpi_compute_parameters()
+            quote = self.env['sale.order'].search_count([('state', '=', 'sale'), ('date_order', '>=', start), ('date_order', '<', end)])
+            record.kpi_quote_value = quote
 
-        account_journal = account.account_journal()
-        
-        bank_account = account_journal.get_journal_dashboard_datas['account_balance']
+    # KPI
+    kpi_new_client = fields.Boolean('Confirmed sales to new clients')
+    kpi_new_client_value = fields.Integer(compute="_compute_kpi_new_client_value")
+
+    def _compute_kpi_new_client_value(self):
+
+        start_datetime = fields.Datetime.to_string(self._context.get('start_datetime') + relativedelta(days=-1))
+        end_datetime = fields.Datetime.to_string(self._context.get('end_datetime') + relativedelta(days=+1))
+        client_ids = self.env['res.partner'].search_count([('create_date', '>=', start_datetime), ('create_date', '<=', end_datetime)])
+
+        if type(client_ids) == int:
+            client_ids = [client_ids]
 
         for record in self:
             start, end, company = record._get_kpi_compute_parameters()
-            # bank_account = self.env['sale.order'].search_count([('state', '=', 'sent'), ('date_order', '>=', start), ('date_order', '<', end)])
-            record.kpi_bank_account_value =
-        """
-        
-        # print(self)
+            new_client = self.env['sale.order'].search_count([('state', '=', 'sale'), ('date_order', '>=', start), ('date_order', '<', end), ('partner_id', 'in', client_ids)])
+            record.kpi_new_client_value = new_client
 
-        """
-        
-        balances = []
-
-        accounts = self.env['account.journal'].search([['type', '=', 'bank']])
-
-        for account in accounts:
-        
-            balances.append([
-                account.name,
-                account._get_journal_bank_account_balance()[0]
-            ])
-        
-        # print(balances)
-        
-
-        # self.kpi_bank_account_value = 789
-
-        return balances
-
-        """
-
+    # KPI
+    def _compute_kpi_bank_account_value(self):
         return [[x.name, '$' + "{0:,.2f}".format(x._get_journal_bank_account_balance()[0])] for x in self.env['account.journal'].search([['type', '=', 'bank']])]
 
+    # KPI
     def _compute_leaderboard_value(self, start):
 
         leaderboard = []
-
         salespeople = self.env['res.users'].search([['sale_team_id', '!=', False]])
 
-        # print(salespeople)
-
         for salesperson in salespeople:
-
-            # print(self.env['res.partner'].search([['user_id', '=', salesperson.id]]).name)
-            # print(len(self.env['res.partner'].search([['user_id', '=', salesperson.id]])))
-
-            # print(salesperson.id)
-            # print(len(self.env['sale.order'].search([['user_id', '=', salesperson.id]])))
-
             leaderboard.append([
-                # self.env['res.partner'].search([['user_id', '=', salesperson.id]]).name
-                # salesperson.id
-                # self.env['res.users'].search([['sale_team_id', '!=', False]])[0].partner_id.name
                 salesperson.partner_id.name
                 ,
-                # str(len(self.env['sale.order'].search([['user_id', '=', salesperson.id]]))) + ' sales'
-                # len(self.env['sale.order'].search([['user_id', '=', salesperson.id]]))
                 sum(sale_order.amount_total for sale_order in self.env['sale.order'].search([
                     ['user_id', '=', salesperson.id],
                     ['date_order', '>=', start],
                     ['date_order', '<=', datetime.now()],
                 ]))
             ])
-
-            # unsorted_list.sort(key=lambda x: x[3])
             leaderboard.sort(key=lambda x: x[1])
             leaderboard.reverse()
 
@@ -173,53 +98,36 @@ class Digest(models.Model):
 
         return leaderboard
 
-    # NUEVO 2023 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
-
-    """
-    kpi_test = fields.Boolean('Test KPI')
-    kpi_test_value = fields.Integer(compute="_compute_kpi_test_value")
-    def _compute_kpi_test_value(self):
-        self.kpi_test_value = 123
-    """
+    # KPI
 
     """
     1. Cliente que nunca haya comprado
     Este puede ser un cliente que sea totalmente nuevo o que ya se haya guardado su contacto pero nunca compró. 
     En este caso, si fueramos a poner esta info, se pondría también en la sección de clientes nuevos?
     """
+
     kpi_clients_with_no_sales = fields.Boolean('Clientes with no sales')
     kpi_clients_with_no_sales_value = fields.Integer(compute="_compute_clients_with_no_sales")
+
     def _compute_clients_with_no_sales(self):
         
         client_ids = []
-        # return clients
-        # self.kpi_clients_with_no_sales_value = 0
-
         start_datetime = fields.Datetime.to_string(self._context.get('start_datetime'))
         end_datetime = fields.Datetime.to_string(self._context.get('end_datetime'))
-
-        # print(start_datetime)
-        # print(end_datetime)
-
         clients = self.env['res.partner'].search([])
-
-        # print(clients)
-        
         temporal = 0
 
         for client in clients:
-            
-            # client_ids.append(client.id)
 
-            client_orders = self.env['sale.order'].search_count([('state', '=', 'sent'), ('date_order', '>=', start_datetime), ('date_order', '<', end_datetime), ('partner_id', 'in', [client.id])])
+            client_orders = self.env['sale.order'].search_count([('state', '=', 'sale'), ('date_order', '>=', start_datetime), ('date_order', '<', end_datetime), ('partner_id', 'in', [client.id])])
 
             if type(client_orders) == int:
                 if client_orders == 0:
                     temporal += 1
 
-        # print(client_ids)
-
         self.kpi_clients_with_no_sales_value = temporal
+
+    # KPI
 
     """
     2. Cliente que ha sido registrado en el transcurso del mes
@@ -232,49 +140,20 @@ class Digest(models.Model):
 
         start_datetime = fields.Datetime.to_string(self._context.get('start_datetime'))
         end_datetime = fields.Datetime.to_string(self._context.get('end_datetime'))
-
         clients = self.env['res.partner'].search([('create_date', '>=', start_datetime), ('create_date', '<=', end_datetime)])
-
-        # print('\/')
-        # print(clients)
-        # print(type(clients))
-        # print('^')
-
         clients_amount = len(clients)
-
-        # print('\/')
-        # print(clients_amount)
-        # print(type(clients_amount))
-        # print('^')
-
         temporal = 0
 
         for client in clients:
-            client_orders = self.env['sale.order'].search_count([('state', '=', 'sent'), ('date_order', '>=', start_datetime), ('date_order', '<', end_datetime), ('partner_id', 'in', [client.id])])
+            client_orders = self.env['sale.order'].search_count([('state', '=', 'sale'), ('date_order', '>=', start_datetime), ('date_order', '<', end_datetime), ('partner_id', 'in', [client.id])])
             if type(client_orders) == int:
                 if client_orders > 0:
                     temporal += 1
-
         
         self.kpi_clients_with_sales_value = temporal
 
-    """
-    3. el Plus
-    Para este caso, también estás de acuerdo que esa información se coloque en la sección de clientes nuevos?
-    """
-    # SOLO COMENTARIO
-    """
-    En resumen, algunos de los casos de los 3 puntos anteriores no son extrictamente clientes nuevos, estás de acuerdo que las estadísticas caigan dentro de la sección de clientes nuevos?
-    """
-    # 
-    # #### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
-
     def _action_send_to_user(self, user, tips_count=1, consum_tips=True):
-        # print('En el método >def _action_send_to_user(self, user, tips_count=1, consum_tips=True):< heredado')
-        # print(self._compute_kpi_bank_account_value())
-        # print(self._compute_leaderboard_value())
         rendered_body = self.env['mail.render.mixin'].with_context(preserve_comments=True)._render_template(
-            # 'digest.digest_mail_main',
             'compounding_mexico_digest_kpis.digest_mail_main_compounding_mexico',
             'digest.digest',
             self.ids,
@@ -299,7 +178,6 @@ class Digest(models.Model):
             },
             post_process=True
         )[self.id]
-        # print(rendered_body)
         full_mail = self.env['mail.render.mixin']._render_encapsulate(
             'digest.digest_mail_layout',
             rendered_body,
@@ -308,7 +186,6 @@ class Digest(models.Model):
                 'user': user,
             },
         )
-        # create a mail_mail based on values, without attachments
         mail_values = {
             'auto_delete': True,
             'author_id': self.env.user.partner_id.id,
