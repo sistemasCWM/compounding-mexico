f7fb48054a5ea3de7b1bbb2fca6a6b3474fb156d
sistemasCWM <114523452+sistemasCWM@users.noreply.github.com>
2023-05-03 13:55:00 -0700

    Delete estafeta_odoo_integration directory

--- estafeta_odoo_integration/models/product_packing.py
+++
@@ -1,5 +0,0 @@
-from odoo import fields, models
-
-class ProductPackaging(models.Model):
-    _inherit = 'product.packaging'
-    package_carrier_type = fields.Selection(selection_add=[("estafeta", "Estafeta")])
