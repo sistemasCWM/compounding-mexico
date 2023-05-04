2f34857f081cce8b996132e9f154c955d5a71eaf
sistemasCWM <sistemas@mexicocompounding.com>
2023-05-04 19:35:04 +0000

    Modificacion 1.0

--- cwm/__manifest__.py
+++ cwm/__manifest__.py
@@ -10,13 +10,13 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Compounding Wholesalers of Mexico",
+    'website': "http://www.mexicocompounidng.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Estadisticos',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
