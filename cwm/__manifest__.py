c04bbbafa9bb34b1be5143f11615a982e170ecf6
sistemasCWM <sistemas@mexicocompounding.com>
2023-05-08 17:51:58 +0000

    cambios 08may

--- cwm/__manifest__.py
+++ cwm/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "Compounding Wholesalers of Mexico",
-    'website': "http://www.mexicocompounidng.com",
+    'website': "http://www.mexicocompounding.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
